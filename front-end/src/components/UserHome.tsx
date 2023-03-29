9d446a648d07a5ac9e0b54f1d95d07019275efc8
Princess Tamayao <princess@bpoc.co.jp>
2023-03-29 11:34:45 +0800

    changed console.log to alert instead

--- front-end/src/components/UserHome.tsx
+++ front-end/src/components/UserHome.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect} from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import {baseURL, client} from "../config/AxiosConfig";
 import { decodeToken } from "react-jwt";
 import Cookies from 'universal-cookie';
@@ -7,21 +7,30 @@ import {MainHeader} from "../headers/MainHeader";
 
 const UserHome = () => {
 
+
+  const navigate = useNavigate();
+
   const cookies = new Cookies();
   let user_token = cookies.get('user_token')
-  
+
   const myDecodedToken = decodeToken(user_token+"");
   const decodeUserToken = JSON.parse( JSON.stringify(myDecodedToken) );
+
+  const logOut = () => {
+    navigate("/logout");
+  };
+
 
   const payloadToken = {token: user_token};
 
   const validateToken = async () => {
-    const response: any = await client.post(baseURL+`/users/requirejwt`, payloadToken);
-    if(response.status===200) {
-      const res= response.data;
-      console.log(res);
+    const headers = {'Authorization': 'Bearer '+user_token}
+    const response: any = await client.post(baseURL+`/users/requirejwt`, payloadToken,{headers:headers});
+    if(response.status===204) {
+      alert("Successfully Logged In");
     } 
   }
+
   useEffect(() => {
     validateToken();
   },[]);
